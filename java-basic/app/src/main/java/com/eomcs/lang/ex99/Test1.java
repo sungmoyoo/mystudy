97e0328ee8d948985894b83216ae46d3ee6fd13c
sungmoyoo <samyoo644@gmail.com>
2023-11-29 20:40:56 +0900

    Day12

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex99/Test1.java
@@ -0,0 +1,20 @@
+package com.eomcs.lang.ex99;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+  public static void main(String[] args) throws Exception {
+    // java.io.InputStream in = System.in;
+    // java.util.Scanner keyIn = new java.util.Scanner(in);
+    Scanner keyIn = new Scanner(System.in);
+    String str = keyIn.nextLine();
+    System.out.println("===> " + str);
+
+    keyIn.close();
+
+  }
+
+
+
+}
--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,30 +1,68 @@
 package bitcamp.myapp;
 
+import java.util.Scanner;
 
 public class App {
   
+  // 애플리케이션 객체 App을 실행할 때 다음 변수를 미리 준비해준다.
+  static final String RESET = "\033[0m";
+  static final String RED_BOLD = "\033[1;31m";
+  static final String APP_TITLE = RED_BOLD + "[과제 관리 시스템]" + RESET;
+  static final String[] MENU_LIST = {
+      APP_TITLE,
+      "-------------------",
+      "1. 과제",
+      "2. 게시글",
+      "3. 도움말",
+      "4. 종료",
+      "-------------------"
+  };
+  static final Scanner keyIn = new Scanner(System.in);
   
   public static void main(String[] args) {
-    String RESET = "\033[0m";
-    String RED_BOLD = "\033[1;31m";
-    String RED_NORMAL = "\033[31m";
+    printMenu();
     
-    String appTitle = "[과제 관리 시스템]";
-    String menu1 = "1. 과제",
-        menu2 = "2. 게시글",
-        menu3 = "3. 도움말",
-        menu4 = "4. 종료";
-    
-    System.out.println(RED_BOLD + appTitle + RESET);
-    System.out.println("-------------------");
-    System.out.println();
-    System.out.println(menu1);
-    System.out.println(menu2);
-    System.out.println(menu3);
-    System.out.println(RED_NORMAL + menu4 + RESET);
-    System.out.println("-------------------");
-    
-    int[] a = new int[3];
-    System.out.println(a[0]);
+    label:
+    while (true) {
+      switch (prompt()) {
+        case "1":
+          System.out.println("과제입니다.");
+          System.out.println();
+          break;
+        case "2":
+          System.out.println("게시글입니다.");
+          System.out.println();
+          break;
+        case "3":
+          System.out.println("도움말입니다.");
+          System.out.println();
+          break;
+        case "4":
+          System.out.println("종료합니다.");
+          break label;
+        case "menu":
+          printMenu();
+          break;
+        default:
+          System.out.println("잘못된 번호입니다.");
+          System.out.println();
+          break;
+      }
+    }
+    keyIn.close();
+  }
+  
+  // ANSI 코드와 App제목, 메뉴를 저장한 변수를 메서드 안에 두는 대신에 클래스 블록 안에 두면
+  // printMenu()를 호출할 때마다 변수를 만들지 않기 때문에 실행 속도나 메모리 부분에서 훨씬 효율적이다.
+  // 보통 메서드 호출될 때마다 값이 바뀌는 변수가 아니라 고정 값을 갖는 변수인 경우 메서드 밖에 두는 것이 좋다.
+  static void printMenu() {
+    for (String menu : MENU_LIST) {
+      System.out.println(menu);
+    }
+  }
+  
+  static String prompt() {
+    System.out.print("> ");
+    return keyIn.nextLine();
   }
 }
